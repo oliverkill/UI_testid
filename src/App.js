2f31d28bce88d5c947c3c7cf194d72f7d70392df
oliverkill <killoliver08@gmail.com>
2021-05-18 13:26:53 +0300

    ui test commit-a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import store from "./store";
+import RestaurantScreen from "./component/RestaurantScreen";
+import { createMuiTheme } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
+const App = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>learn react & ui testing</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <RestaurantScreen />
+      </Container>
+    </ThemeProvider>
+  </Provider>
+);
 
 export default App;
